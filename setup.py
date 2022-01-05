f50f165c3121ff5c88b841a72efeb488c859c7b8
carnarez <11967125+carnarez@users.noreply.github.com>
2022-01-05 13:56:38 +0100

    Make it a package

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setuptools.setup(
     description="Add support for file inclusion to Python-Markdown.",
     install_requires=["markdown"],
     name="markdown-insert",
-    package_data={"": ["*.pyi"]},
-    py_modules=["markdown_insert"],
+    packages=["markdown_insert"],
+    package_data={"markdown_insert": ["*.pyi", "py.typed"]},
     url="https://github.com/carnarez/markdown-insert",
     version="0.0.1",
 )
